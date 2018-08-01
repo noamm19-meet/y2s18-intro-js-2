faf20832698d76463f0f2626325ea00c5c7249e3
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:16:37 +0300

    json

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,11 @@
 // Create your 'me' object literal here!
+var Me=
+{
+	firstName:"Noam",
+	lastName:"Mertsen",
+	favFood:["fruits","soop","pizza"],
+	age:16,
+	picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhNeGpSCcCZ-z0K-vbHF__L20ausjs-8-FK5YYVcgCVtRxcY8d",
+}
+
+document.write(JSON.stringify(Me));	
