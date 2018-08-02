6815b45ac071b8a7a0d479a949f8492af74052f2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:25:37 +0300

    eventListener

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,24 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
-var currentIndex = 0;
+// var pictures = ['./imgs/dog.jpg'];
+// var currentIndex = 0;
 
-function showNextPicture() {
-  currentIndex++; // increment current picture
-  // if currentIndex is too large, start from the beginning again
-  if (currentIndex >= pictures.length) {
-    currentIndex = 0;
+// function showNextPicture() {
+//   currentIndex++; // increment current picture
+//   // if currentIndex is too large, start from the beginning again
+//   if (currentIndex >= pictures.length) {
+//     currentIndex = 0;
+//   }
+  function alert12()
+  {
+  	alert("Good image");
   }
+  var picture=document.getElementsByTagName("img");
+  picture[0].addEventListener("click", function (event)
+  {
+  	alert12();
+  }
+  ); 	
+
 
   // YOUR TASK: Finish this function!
-}
+// }
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,5 @@
 // Put all your JavaScript in this file!
+function promptMe()
+{
+	prompt("which color you want?");
+}
